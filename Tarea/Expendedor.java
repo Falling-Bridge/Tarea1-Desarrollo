633788b2e40ab2ea5101940292411d213f8222bf
Falling-Bridge <lucmorales2023@inf.udec.cl>
2024-04-25 18:34:05 -0400

    El código funciona, pero no como debería; porque me tira las exceptiones incorrectas

--- Tarea/Expendedor.java
+++ Tarea/Expendedor.java
@@ -12,32 +12,31 @@ class Expendedor {
     private Deposito<Dulces> calugas;
     private Deposito<Dulces> oreos;
     private Deposito<Moneda> monVu;
-    private int price;
 
     public Moneda getVuelto() throws Exception{
         return monVu.getElemento();
     }
 
-    public Producto comprarProducto(Moneda m, int cual) throws Exception{
+    public Producto comprarProducto(Moneda m, Valoresestaticos producto) throws Exception{
         if(m == null){
             throw new PagoIncorrectoException();
         }
 
-        if(m.getValor()<price || (cual != 1 && cual != 2 && cual != 3 && cual != 4 && cual != 5 && cual != 6)){
+        if( m.getValor() < producto.getCoste()){
             monVu.addElemento(m);
             throw new PagoInsuficienteException();
         }
         
-        for(int i = price; i<m.getValor(); i+=100){
+        for(int i = producto.getCoste(); i<m.getValor(); i+=100){
             monVu.addElemento(new Moneda100());
         }
         Bebida auxBebida = null;
         Dulces auxDulces = null;
 
-        switch (cual) {
-            case 1: 
+        switch (producto) {
+            case COCA: 
                 if (coca.getElemento() == null){
-                    for (int i = price; i < m.getValor(); i += 100){
+                    for (int i = producto.getCoste(); i < m.getValor(); i += 100){
                         monVu.addElemento(m);
                     }
                     monVu.addElemento(m);
@@ -45,49 +44,49 @@ class Expendedor {
                 }
                 return coca.getElemento();
 
-            case 2: 
+            case SPRITE: 
                 auxBebida = sprite.getElemento();
                 if(auxBebida == null){
-                    for(int i = price; i<m.getValor(); i+=100){
+                    for(int i = producto.getCoste(); i<m.getValor(); i+=100){
                         monVu.getElemento();
                     }
                     monVu.addElemento(m);
                 }
                 return auxBebida;
-            case 3: 
+            case PEPSI: 
                 auxBebida = pepsi.getElemento();
                 if(auxBebida == null){
-                    for(int i = price; i<m.getValor(); i+=100){
+                    for(int i = producto.getCoste(); i<m.getValor(); i+=100){
                         monVu.getElemento();
                     }
                     monVu.addElemento(m);
                 }
                 return auxBebida;
                 
-            case 4: 
+            case SERRANITA: 
                 auxDulces = serranita.getElemento();
                 if(auxDulces == null){
-                    for(int i = price; i<m.getValor(); i+=100){
+                    for(int i = producto.getCoste(); i<m.getValor(); i+=100){
                         monVu.getElemento();
                     }
                     monVu.addElemento(m);
                 }
                 return auxDulces;
 
-            case 5: 
+            case CALUGAS: 
                 auxDulces = calugas.getElemento();
                 if(auxDulces == null){
-                    for(int i = price; i<m.getValor(); i+=100){
+                    for(int i = producto.getCoste(); i<m.getValor(); i+=100){
                         monVu.getElemento();
                     }
                     monVu.addElemento(m);
                 }
                 return auxDulces;
             
-            case 6: 
+            case OREOS: 
                 auxDulces = oreos.getElemento();
                 if(auxDulces == null){
-                    for(int i = price; i<m.getValor(); i+=100){
+                    for(int i = producto.getCoste(); i<m.getValor(); i+=100){
                         monVu.getElemento();
                     }
                     monVu.addElemento(m);
@@ -98,7 +97,7 @@ class Expendedor {
         }
     }
 
-    public Expendedor(int a, int b) {
+    public Expendedor(int a) {
         coca = new Deposito<Bebida>();
         sprite = new Deposito<Bebida>();
         pepsi = new Deposito<Bebida>();
@@ -123,6 +122,5 @@ class Expendedor {
                 oreos.addElemento(ore);
             }
         }
-        price = b;
     }
 }
--- Tarea/Main.java
+++ Tarea/Main.java
@@ -1,13 +1,20 @@
 package Tarea;
 import Tarea.clasemoneda.*;
+import Tarea.mensajeerror.*;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        Expendedor exp = new Expendedor(3, 500);
+        Expendedor exp = new Expendedor(3);
         Moneda m = null;
         Comprador c = null;
+        Valoresestaticos v = null;
+        v = Valoresestaticos.SERRANITA;
         m = new Moneda1500();
-        c = new Comprador(m, 3, exp);
-        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());
+        try{
+            c = new Comprador(m, v, exp);
+            System.out.println(c.queConsumiste() + ", " + c.cuantoVuelto());
+        }catch(NoHayProductoException | PagoIncorrectoException e){
+            System.out.println("holas: 2 "+ e.getMessage());  
+        }
     }
 }
